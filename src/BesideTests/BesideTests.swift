b66f50a44afcd6b7dda6d8f6b5a54ba8843295ee
wubaolai <wubaolai@onething.net>
2018-11-30 14:27:22 +0800

    add:加一些初始文件

---
+++ src/BesideTests/BesideTests.swift
@@ -0,0 +1,34 @@
+//
+//  BesideTests.swift
+//  BesideTests
+//
+//  Created by wubaolai on 2018/11/30.
+//  Copyright © 2018 wubaolai. All rights reserved.
+//
+
+import XCTest
+@testable import Beside
+
+class BesideTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
