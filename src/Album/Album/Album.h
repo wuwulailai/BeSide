58bf4ae73b01faa40d653af638f70ec43027c603
wubaolai <wubaolai@onething.net>
2018-12-03 11:03:15 +0800

    add: .gitignore

---
+++ src/Album/Album/Album.h
@@ -0,0 +1,19 @@
+//
+//  Album.h
+//  Album
+//
+//  Created by fuchen on 2018/12/1.
+//  Copyright © 2018 fuchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Album.
+FOUNDATION_EXPORT double AlbumVersionNumber;
+
+//! Project version string for Album.
+FOUNDATION_EXPORT const unsigned char AlbumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Album/PublicHeader.h>
+
+
---
+++ src/Dlna/Dlna/Dlna.h
@@ -0,0 +1,19 @@
+//
+//  Dlna.h
+//  Dlna
+//
+//  Created by fuchen on 2018/12/1.
+//  Copyright © 2018 fuchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dlna.
+FOUNDATION_EXPORT double DlnaVersionNumber;
+
+//! Project version string for Dlna.
+FOUNDATION_EXPORT const unsigned char DlnaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dlna/PublicHeader.h>
+
+
